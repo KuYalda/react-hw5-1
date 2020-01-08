1cfca872532b791d32b42fa998771538a01c0843
Igor Ozheredov <o.igorok@gmail.com>
2020-01-08 18:33:37 +0200

    Updates

---
+++ precache-manifest.dee57865d05ce5956c3fc621486b69e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "29939a0372e81e086fe2",
+    "url": "/react-hw5-1/static/css/main.d4ed6fcb.chunk.css"
+  },
+  {
+    "revision": "29939a0372e81e086fe2",
+    "url": "/react-hw5-1/static/js/main.c46e28f4.chunk.js"
+  },
+  {
+    "revision": "b00539d2b1111e33de70",
+    "url": "/react-hw5-1/static/js/runtime~main.e992420d.js"
+  },
+  {
+    "revision": "b46629b7c74273237f67",
+    "url": "/react-hw5-1/static/js/2.64544461.chunk.js"
+  },
+  {
+    "revision": "b5b3befba5d349e179ce6b238561c6a9",
+    "url": "/react-hw5-1/index.html"
+  }
+];
